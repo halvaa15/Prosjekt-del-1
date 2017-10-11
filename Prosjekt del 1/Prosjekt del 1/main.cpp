68d7df22b7b75e896aab49ea526801069be3286a
Halvard Yri Adriaenssens <halvard@adriaenssens.no>
2017-10-11 10:58:10 +0200

    Kompilerbart blinkeprogram

--- Prosjekt del 1/Prosjekt del 1/main.cpp
+++ Prosjekt del 1/Prosjekt del 1/main.cpp
@@ -5,14 +5,27 @@
  * Author : halva
  */ 
 
-#include <avr/io.h>
+#ifndef F_CPU
+#define F_CPU 1000000UL
+#endif
 
+#include <avr/io.h>		//Headerfil som gir oss forskjellige input/output operasjoner
+#include <util/delay.h>		//Bibliotek for delay-funksjoner
 
 int main(void)
 {
-    /* Replace with your application code */
+	DDRD = 0b00000001;	//DDR = data direction. B = område på mirkokontroleren. Her setter vi pinne 0 på området D til å være en utgang.
+	PORTD = 0b00000000;
+	
     while (1) 
     {
+		//Skru på lyset
+		PORTD |= 0b00000001;		//Setter pinnen PD0 til å være høy
+		_delay_ms(100);
+		
+		//Skru av lyset
+		PORTD &= 0b11111110;		//Setter pinnen til å være lav
+		_delay_ms(100);
     }
 }
 
