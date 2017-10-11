924f0bd9f8b6a69d441b8a3aef3a08a05b416314
Halvard Yri Adriaenssens <halvard@adriaenssens.no>
2017-10-11 10:39:26 +0200

    Kompilerbart program
    Åpnet ett nytt prosjekt i AtmelStudio

---
+++ Prosjekt del 1/Prosjekt del 1/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Prosjekt del 1.cpp
+ *
+ * Created: 11.10.2017 10.36.31
+ * Author : halva
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
